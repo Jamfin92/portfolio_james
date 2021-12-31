d794799a67b006adf5d99950c9e1a4cea3c065eb
Jamfin92 <36896551+Jamfin92@users.noreply.github.com>
2021-12-31 18:08:54 -0500

    Completed TimeLine.js, Accomplishments.js, Footer.js. Added "data" const to constants.js from Accomplishments.js.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+        <LinkList>
+            <LinkColumn>
+                <LinkTitle>Call</LinkTitle>
+                <LinkItem href="tel:914-336-6843">(914)336-6843</LinkItem>
+            </LinkColumn>
+            <LinkColumn>
+                <LinkTitle>Email</LinkTitle>
+                <LinkItem href="mailto:jamfin92@gmail.com">jamfin92@gmail.com</LinkItem>
+            </LinkColumn>
+        </LinkList>
+        <SocialIconsContainer>
+            <CompanyContainer>
+                <Slogan>It's not in the name. It's the action behind it.</Slogan>
+            </CompanyContainer>
+            <SocialContainer>
+                <SocialIcons href="https://github.com/jamfin92">
+                    <AiFillGithub size="3rem"/>
+                </SocialIcons>
+                <SocialIcons href="https://linkedin.com/Jamfin92">
+                    <AiFillLinkedin size="3rem"/>
+                </SocialIcons>
+                <SocialIcons href="https://instagram.com">
+                    <AiFillInstagram size="3rem"/>
+                </SocialIcons>
+            </SocialContainer>
+        </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
